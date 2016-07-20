13feb08f0f21bd85cecbef3235f4011377994409
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-07-20 10:41:31 +0100

    start cli.

--- src/io/stpettersen/bluejpackager/BlueJPackager.java
+++ src/io/stpettersen/bluejpackager/BlueJPackager.java
@@ -6,9 +6,14 @@ Copyright (c) 2016 Sam Saint-Pettersen.
 Released under the MIT License.
 */
 package io.stpettersen.bluejpackager;
+import org.apache.commons.cli.*;
 
 public class BlueJPackager {
     public static void main(String[] args) {
-        System.out.println("TODO!");
+        // Create the command line parser.
+        CommandLineParser parser = new DefaultParser();
+        // Create the command line options.
+        Options options = new Options();
+        
     }
 }
