d960ebdf9135fbb0cbcb125bdd117eb60f71c46e
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2016-07-31 12:16:11 +0100

    ...

--- src/io/stpettersen/bluejpackager/BlueJPackager.java
+++ src/io/stpettersen/bluejpackager/BlueJPackager.java
@@ -1,6 +1,6 @@
 /*
 BlueJ Packager.
-Utility to package flat structure Blue J project into a structured package.
+Utility to morph a BlueJ project into a structured Java package.
 
 Copyright (c) 2016 Sam Saint-Pettersen.
 Released under the MIT License.
@@ -9,10 +9,29 @@ package io.stpettersen.bluejpackager;
 import org.apache.commons.cli.*;
 
 public class BlueJPackager {
+
+    private static void displayUsage() {
+        System.out.println("\nBlueJ Packager.");
+        System.out.println("Utility to morph a BlueJ project into a structured Java package.");
+        System.out.println("\nUsage: blpackager");
+    }
+
     public static void main(String[] args) {
         // Create the command line parser.
         CommandLineParser parser = new DefaultParser();
+
         // Create the command line options.
         Options options = new Options();
+        options.addOption("h", "help", false, "Display this help information and exit.");
+
+        try {
+            // Parse the command line arguments.
+            CommandLine cli = parser.parse(options, args);
+            if(cli.hasOption("h")) displayUsage();
+        }
+        catch(Exception e) {
+            // Bad command line option occurred.
+            System.err.println(e.getMessage());
+        }
     }
 }
